851bac3983ef017602e8bf8d99bb8569c7200959
Zijie Cai <leo20020208@gmail.com>
2025-10-27 13:25:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/multilingual-coporate-website/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
